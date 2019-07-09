17f90509bd062d5d181ec3be7341dcf7d30294d7
leeggco <leeggco@gmail.com>
2019-07-09 17:06:26 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const Hapi = require('hapi');
+require('env2')('./.env');
+const config = require('./config');
+const routesHelloHapi = require('./routes/hello-hapi');
+const routesShops = require('./routes/shops');
+const routesOrders = require('./routes/orders');
+// 引入自定义的 hapi-swagger 插件配置
+const pluginHapiSwagger = require('./plugins/hapi-swagger');
+const init = async () => {
+  const server = Hapi.Server({
+    port: config.port,
+    host: config.host,
+  });
+  // 使用插件
+  // await server.register(require('good'));
+  await server.register(require('inert'));
+  await server.register({
+    plugin: require('hapi-pino'),
+    options: {
+      prettyPrint: false,
+      logEvents: ['response', 'onPostStart']
+    }
+  });
+  await server.register([
+    // 为系统使用 hapi-swagger
+    ...pluginHapiSwagger
+  ]);
+  // 普通GET请求
+  server.route([
+    ...routesHelloHapi,
+    ...routesShops,
+    ...routesOrders,
+  ]);
+  // 启动服务器
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+
+  console.log(err);
+  process.exit(1);
+});
+
+init();
---
+++ routes/hello-hapi.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return 'Hello World!';
+    },
+    config: {
+      tags: ['api', 'tests'],
+      description: '测试hello-hapi',
+    },
+  }, {
+    method: 'GET',
+    path: '/{name}',
+    handler: (request, h) => {
+      return ('Hello, ' + encodeURIComponent(request.params.name) + "!");
+    }
+  }, {
+    method: 'GET',
+    path: '/hello',
+    handler: (request, h) => {
+      return h.file('./hello.html')
+    }
+  }
+
+]
